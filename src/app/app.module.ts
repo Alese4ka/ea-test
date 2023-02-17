a69f988db76d1b93bcef30e4dde4e1251a1ab2c9
Alesia Branavitskaya <alesun4ik@gmail.com>
2023-02-17 22:53:13 +0300

    add material library, add logic for counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, MainPageComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    FormsModule,
+    MatFormFieldModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/main-page/main-page.component.ts
@@ -0,0 +1,46 @@
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { interval, min, Observable, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.scss'],
+})
+export class MainPageComponent implements OnInit {
+  public finalDate: number = new Date(2023, 4, 31).getTime();
+  public countDownDate!: string;
+
+  dateInterval = setInterval(() => {
+    const now: number = new Date().getTime();
+    const diff: number = this.finalDate - now;
+    const days: number = Math.floor(diff / (1000 * 60 * 60 * 24));
+    let hours: number | string = Math.floor(
+      (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    let minutes: number | string = Math.floor(
+      (diff % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    let sec: number | string = Math.floor((diff % (1000 * 60)) / 1000);
+
+    if (hours < 10) {
+      hours = '0' + hours;
+    }
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+    if (sec < 10) {
+      sec = '0' + sec;
+    }
+
+    this.countDownDate = days + ' : ' + hours + ' : ' + minutes + ' : ' + sec;
+
+    if (diff < 0) {
+      clearInterval(this.dateInterval);
+      this.countDownDate = 'Expired';
+    }
+  });
+
+  public ngOnInit(): void {
+    console.log(new Date(2023, 4, 31));
+  }
+}
